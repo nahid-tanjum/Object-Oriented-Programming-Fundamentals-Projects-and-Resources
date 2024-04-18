26ccae8e974ecdcf3fbd13915bb4e18594291abc
Md Nahid Tanjum <165870804+nahid-tanjum@users.noreply.github.com>
2024-04-19 01:02:18 +1000

    Swin_adventure

---
+++ IdentifiableObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swin_Adventure
+{
+    public class IdentifiableObject
+    {
+        private List<string> _identifiers = new List<string>();
+
+        public IdentifiableObject(string[] idents)
+        {
+            foreach (string ident in idents)
+            {
+                _identifiers.Add(ident);
+            }
+        }
+
+        public bool AreYou(string elem)
+        {
+            foreach (string ident in _identifiers)
+            {
+                if (elem.ToLower() == ident)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string FirstId
+        {
+            get
+            { 
+                if (_identifiers.Count == 0)
+                {
+                    return "";
+                }
+                else
+                {
+                    return _identifiers[0];
+                }
+            }
+        }
+
+        public void AddIdentifier(string ident)
+        {
+            _identifiers.Add(ident.ToLower());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,37 +1,10 @@
-﻿// Import required namespaces
-using System;
-using System.Collections.Generic;
-
-// Declare the OOP_Exam namespace
-namespace OOP_Exam
+﻿namespace Swin_Adventure
 {
-    // Define the Program class
-    class Program
+    internal class Program
     {
-        // Define the entry point of the program
         static void Main(string[] args)
         {
-            // Create a DataAnalyser object with a list of integers representing the individual digits of a student ID number
-            Console.WriteLine("The following minimum and maximum number we get from my student ID's number");
-            var test = new DataAnalyser(new List<int> { 1, 0, 3, 8, 0, 7, 0, 6, 8 }, new MinMaxSummary());
 
-            // Call the Summarise method to calculate and output the minimum and maximum numbers from the data
-            test.Summarise();
-
-            // Add three more numbers to the data analyser
-            test.AddNumber(19);
-            test.AddNumber(99);
-            test.AddNumber(79);
-            Console.WriteLine("The Following average number we get by adding three random number 19, 99, 79");
-
-            // Set the summary strategy to the average strategy
-            test.Strategy = new AverageSummary();
-
-            // Call the Summarise method again to calculate and output the average of all numbers in the data
-            test.Summarise();
-
-            // Wait for user input before closing the program
-            Console.ReadKey();
         }
     }
 }
