0f4b3dcfc960b514f12f72aa1a003626983a55b1
Md Nahid Tanjum <165870804+nahid-tanjum@users.noreply.github.com>
2024-04-19 01:09:09 +1000

    Splashkit_demo

--- Program.cs
+++ Program.cs
@@ -1,49 +1,11 @@
 using System;
 using SplashKitSDK;
 
-namespace ShapeDrawer
+public class Program
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main()
-        {
-            Window window = new Window("Shape Drawer", 800, 600);
-
-            Drawing drawing = new Drawing();
-
-            do
-            {
-                SplashKit.ProcessEvents();
-                SplashKit.ClearScreen();
-
-                if (SplashKit.KeyTyped(KeyCode.SpaceKey))
-                {
-                    drawing.Background = SplashKit.RandomColor();
-                }
-                if (SplashKit.MouseClicked(MouseButton.LeftButton))
-                {
-                    Shape shape = new Shape();
-                    drawing.AddShape(shape);
-                    shape.X = SplashKit.MouseX();
-                    shape.Y = SplashKit.MouseY();
-                }
-                if (SplashKit.MouseClicked(MouseButton.RightButton))
-                {
-                    drawing.SelectShapeAt(SplashKit.MousePosition());
-                }
-                if (SplashKit.KeyTyped(KeyCode.DeleteKey))
-                {
-                    foreach(Shape shape in drawing.SelectedShapes)
-                    {
-                        drawing.RemoveShape(shape);
-                    }
-                }
-
-                drawing.Draw();
-
-                SplashKit.RefreshScreen();
-            } while (!window.CloseRequested);
-
-        }
+        SplashKit.OpenWindow("Test Window", 800, 600);
+        SplashKit.Delay(5000);
     }
 }
